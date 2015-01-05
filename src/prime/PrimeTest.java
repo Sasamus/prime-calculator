28a5784f3757b977918c18aa08b6e6ef2ada44b6
Sasamus <labus93@gmail.com>
2015-01-05 13:10:12 +0100

    Created Class PrimeTest

---
+++ src/prime/PrimeTest.java
@@ -0,0 +1,25 @@
+package prime;
+
+/**
+ * @author Albin Engström
+ */
+public class PrimeTest {
+
+	/**
+	 * Tests if a number is a prime
+	 * 
+	 * @param number
+	 *            The number to test
+	 * @return true if number is a prime, else false
+	 */
+	public static boolean isPrime(long number) {
+		long limit = (long) Math.sqrt(number) + 1;
+		for (long i = 2; i < limit; ++i) {
+			if ((number % i) == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
